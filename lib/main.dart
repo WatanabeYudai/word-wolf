3078ce693908aae6a7187f7247cb570923120413
yudai0308 <lock0308@gmail.com>
2021-12-30 00:07:25 +0900

    微修正

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Word Wolf',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
