4909959397e159e416e85622eda394685571579c
yudai0308 <lock0308@gmail.com>
2022-01-31 22:42:52 +0900

    firebase の向け先を変更

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const databaseUrl = 'https://word-wolf-14ead-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  static const databaseUrl = 'https://word-wolf-785a7-default-rtdb.asia-southeast1.firebasedatabase.app/';
 }
